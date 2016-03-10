aeedeb0296c9fcc12a93952f276f8ee8256a3c81
Clément Renaud <clement.renaud@gmail.com>
2016-03-10 01:20:17 +0100

    clean epub metadata

--- bin/create_ebook.sh
+++ bin/create_ebook.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # create ebook
-gitbook epub .. ./costoffreedom.epub
+gitbook epub . ./costoffreedom.epub
 
 # add relevant metadata
-ebook-meta -a"Collective" -k"Collective" --date="2015-11-07 18:40:14" -p"Book Sprints" --tags="free culture, free software, open source, book sprint, freebassel, Bassel Sadafi" costoffreedom.epub
+ebook-meta -a"Collective" -k"Collective" --date="2015-11-07" -p"Book Sprints" --tags="free culture, free software, open source, book sprint, freebassel, Bassel Sadafi" costoffreedom.epub
